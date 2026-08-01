6dcf5f915fc3fcc76af65d98bafc7c82fedc5e35
likhithaj04 <likhitha15jayram@gmail.com>
2026-08-01 14:47:18 +0530

    implemented retrieval api

--- src/Routes/expenseRoute.js
+++ src/Routes/expenseRoute.js
@@ -1,9 +1,16 @@
 import Router from 'express'
 const router=Router()
-import {addExpense} from '../controllers/expenseController.js'
+import {addExpense,allExpense,deleteExpense,totlExpense} from '../controllers/expenseController.js'
 import { wrapAsync } from '../middlewares/wrapasync.js';
 import { validate } from '../middlewares/zodValidation.js';
 
-router.post("/addExpense",validate,wrapAsync(addExpense));
+router.post("/expense",validate,wrapAsync(addExpense));
+
+router.get("/expense",wrapAsync(allExpense))
+
+router.delete("/expense/:id",wrapAsync(deleteExpense))
+
+router.get("/expense/total",wrapAsync(totlExpense))
+
 
 export default router;
--- src/controllers/expenseController.js
+++ src/controllers/expenseController.js
@@ -1,13 +1,69 @@
 import express from 'express'
+import fs from 'fs/promises'
+import { randomUUID } from 'crypto'
 
-export const addExpense=(req,res)=>{
-    console.log("hit");
-    
- const {title,amount,category,date}=req.body;
+const filePath = "./src/data/expenses.json"
 
+export const addExpense = async (req, res) => {
 
+  const { title, amount, category, date } = req.body;
+  const newExpense = {id:randomUUID(), title, amount, category, date }
 
-  res.send({message:"success",data:{title,amount,category,date}})
+  const data = await fs.readFile(filePath, "utf-8")
+
+  const allExpenses = JSON.parse(data);
+
+  allExpenses.push(newExpense)
+
+  await fs.writeFile(
+    filePath,JSON.stringify(allExpenses, null, 2));
+
+  // console.log(allExpenses);
+
+  res.status(200).json({ message: "success", data: { title, amount, category, date } })
 
 }
 
+
+ export const allExpense=async(req,res)=>{
+   const {category}=req.query;
+   const data=await fs.readFile(filePath,"utf-8")
+  const expense=JSON.parse(data)
+// console.log(expense);
+
+   if(category){
+      const fileterdData=expense.filter(item=>item.category==category)
+         res.status(200).json({"success":true,
+          message:fileterdData.length>0?"Expenses retrieved successfully":"No Expense found for this category",
+          data:fileterdData})
+
+   }
+   else{
+   res.status(200).json({"success":true,data:expense})
+   }
+ 
+}
+
+export const deleteExpense=async(req,res)=>{
+const {id}=req.params
+  const data=await fs.readFile(filePath,"utf-8")
+  const allExpense=JSON.parse(data)
+   
+  const expense=allExpense.find(item=>item.id===id)
+if(expense){
+   const updated=allExpense.filter(item=>item!==expense)   
+    await fs.writeFile(
+    filePath,JSON.stringify(updated, null, 2));
+    res.status(200).json({"success":true,message:"Expense Deleted Successfully"})
+}  
+else{
+  res.status(400).json({"success":false,message:"Data not found"})
+}
+  
+}
+
+export const totlExpense=(req,res)=>{
+  const {category}=req.query;
+  console.log(category);
+  
+}
